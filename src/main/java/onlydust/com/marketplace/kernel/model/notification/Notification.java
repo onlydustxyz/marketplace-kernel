074fbdc736eeeba513a46be536476689f5ac46bd
Olivier Fuxet <olivier.fuxet@gmail.com>
2024-07-27 16:42:12 +0200

    Refactor Notification by adding more fields

--- src/main/java/onlydust/com/marketplace/kernel/model/notification/Notification.java
+++ src/main/java/onlydust/com/marketplace/kernel/model/notification/Notification.java
@@ -1,15 +1,14 @@
 package onlydust.com.marketplace.kernel.model.notification;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.NonNull;
-import lombok.Value;
+import lombok.*;
 import lombok.experimental.Accessors;
 
 import java.time.ZonedDateTime;
+import java.util.Set;
 import java.util.UUID;
 
 @Value
+@Builder
 @AllArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Accessors(fluent = true)
@@ -18,7 +17,23 @@ public class Notification {
     @NonNull
     UUID id;
 
+    @NonNull UUID recipientId;
+
     @NonNull NotificationData data;
 
     @NonNull ZonedDateTime createdAt;
+
+    @NonNull Set<NotificationChannel> channels;
+
+    public static Notification of(@NonNull UUID recipientId,
+                                  @NonNull NotificationData notificationData,
+                                  @NonNull Set<NotificationChannel> channels) {
+        return Notification.builder()
+                .id(UUID.randomUUID())
+                .recipientId(recipientId)
+                .data(notificationData)
+                .createdAt(ZonedDateTime.now())
+                .channels(channels)
+                .build();
+    }
 }

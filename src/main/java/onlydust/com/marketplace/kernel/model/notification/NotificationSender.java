ce30e5e6d99aab521988d989b1e6570e3dfa7a31
Olivier Fuxet <olivier.fuxet@gmail.com>
2024-07-29 11:51:44 +0200

    Remove NotificationSender

--- src/main/java/onlydust/com/marketplace/kernel/model/notification/NotificationSender.java
+++
@@ -1,9 +0,0 @@
-package onlydust.com.marketplace.kernel.model.notification;
-
-/**
- * Implementations typically send pending notification of specific channel(s).
- * Example: EmailNotificationSender, SmsNotificationSender, etc.
- */
-public interface NotificationSender {
-    void sendAll();
-}

8adc9748170a993108fe5ec01f6db04f4c7871aa
Olivier Fuxet <olivier.fuxet@gmail.com>
2024-07-30 11:01:37 +0200

    Add default channels per notification category

--- src/main/java/onlydust/com/marketplace/kernel/model/notification/NotificationCategory.java
+++ src/main/java/onlydust/com/marketplace/kernel/model/notification/NotificationCategory.java
@@ -1,5 +1,7 @@
 package onlydust.com.marketplace.kernel.model.notification;
 
+import java.util.List;
+
 public enum NotificationCategory {
     /** Notify maintainers about their committee applications (eg. created, accepted, etc.) */
     COMMITTEE_APPLICATION_AS_MAINTAINER,
@@ -10,5 +12,9 @@ public enum NotificationCategory {
     /** Notify maintainers about their project applications (eg. contributor applied, etc.) */
     PROJECT_APPLICATION_AS_MAINTAINER,
     /** Notify contributors about specific projects good-first-issues (eg. new one added, etc.) */
-    PROJECT_GOOD_FIRST_ISSUE_AS_CONTRIBUTOR,
+    PROJECT_GOOD_FIRST_ISSUE_AS_CONTRIBUTOR;
+
+    public List<NotificationChannel> defaultChannels() {
+        return List.of(NotificationChannel.IN_APP, NotificationChannel.EMAIL);
+    }
 }

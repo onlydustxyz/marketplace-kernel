bdca9e05e4d7fbd27e3fc7dec098e37b0ae50b65
Olivier Fuxet <olivier.fuxet@gmail.com>
2024-07-31 12:33:58 +0200

    Update notification categories

--- src/main/java/onlydust/com/marketplace/kernel/model/notification/NotificationCategory.java
+++ src/main/java/onlydust/com/marketplace/kernel/model/notification/NotificationCategory.java
@@ -3,16 +3,17 @@ package onlydust.com.marketplace.kernel.model.notification;
 import java.util.List;
 
 public enum NotificationCategory {
-    /** Notify maintainers about their committee applications (eg. created, accepted, etc.) */
-    COMMITTEE_APPLICATION_AS_MAINTAINER,
-    /** Notify contributors about their rewards (eg. received, paid, etc.) */
-    REWARD_AS_CONTRIBUTOR,
-    /** Notify contributors about their project applications (eg. accepted, etc.) */
-    PROJECT_APPLICATION_AS_CONTRIBUTOR,
-    /** Notify maintainers about their project applications (eg. contributor applied, etc.) */
-    PROJECT_APPLICATION_AS_MAINTAINER,
-    /** Notify contributors about specific projects good-first-issues (eg. new one added, etc.) */
-    PROJECT_GOOD_FIRST_ISSUE_AS_CONTRIBUTOR;
+    /** E.g. application to review, etc. */
+    MAINTAINER_PROJECT_CONTRIBUTOR,
+    /** E.g. committee application created, etc. */
+    MAINTAINER_PROJECT_PROGRAM,
+    /** E.g. invoice rejected, reward received/canceled/paid, etc. */
+    CONTRIBUTOR_REWARD,
+    /** E.g. issue application rejected/accepted, new good first issue in project X, etc. */
+    CONTRIBUTOR_PROJECT,
+    /** E.g. KYCB document failed, KYCB complete your profile, etc.  */
+    KYC_KYB_BILLING_PROFILE,
+    ;
 
     public List<NotificationChannel> defaultChannels() {
         return List.of(NotificationChannel.IN_APP, NotificationChannel.EMAIL);

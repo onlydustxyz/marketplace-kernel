7498322891559baeb443a9bdc12e7312fc6ec969
Antho <43467246+AnthonyBuisset@users.noreply.github.com>
2024-08-29 16:07:09 +0200

    create blockchain transaction types (#9)

--- src/main/java/onlydust/com/marketplace/kernel/model/blockchain/aptos/AptosTransaction.java
+++ src/main/java/onlydust/com/marketplace/kernel/model/blockchain/aptos/AptosTransaction.java
@@ -1,12 +1,28 @@
 package onlydust.com.marketplace.kernel.model.blockchain.aptos;
 
-import lombok.EqualsAndHashCode;
-import lombok.NonNull;
+import lombok.*;
+import lombok.experimental.Accessors;
+import lombok.experimental.FieldDefaults;
+import onlydust.com.marketplace.kernel.model.blockchain.Blockchain.Transaction;
 import onlydust.com.marketplace.kernel.model.blockchain.PrefixedHexHash;
 
 import java.time.ZonedDateTime;
 
-public record AptosTransaction(Hash hash, ZonedDateTime timestamp) {
+@AllArgsConstructor
+@EqualsAndHashCode
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
+@Getter
+@Accessors(fluent = true)
+public class AptosTransaction implements Transaction {
+    Hash hash;
+    
+    ZonedDateTime timestamp;
+
+    @Override
+    public String reference() {
+        return hash().toString();
+    }
+
     @EqualsAndHashCode(callSuper = true)
     public static class Hash extends PrefixedHexHash {
         private static final int MAX_BYTE_COUNT = 32;
--- src/main/java/onlydust/com/marketplace/kernel/model/blockchain/stellar/StellarTransaction.java
+++ src/main/java/onlydust/com/marketplace/kernel/model/blockchain/stellar/StellarTransaction.java
@@ -2,11 +2,17 @@ package onlydust.com.marketplace.kernel.model.blockchain.stellar;
 
 import lombok.EqualsAndHashCode;
 import lombok.NonNull;
+import onlydust.com.marketplace.kernel.model.blockchain.Blockchain.Transaction;
 import onlydust.com.marketplace.kernel.model.blockchain.HexHash;
 
 import java.time.ZonedDateTime;
 
-public record StellarTransaction(Hash hash, ZonedDateTime timestamp) {
+public record StellarTransaction(Hash hash, ZonedDateTime timestamp) implements Transaction {
+    @Override
+    public String reference() {
+        return hash().toString();
+    }
+
     @EqualsAndHashCode(callSuper = true)
     public static class Hash extends HexHash {
         private static final int MAX_BYTE_COUNT = 32;

7c138cc7de3567fb870f5128c9e4c7af37e6ed11
Olivier Fuxet <ofux@users.noreply.github.com>
2024-12-16 14:16:09 +0100

    Add PostgresOutboxAdapter to the kernel (includes change to add the PROCESSING status) (#26)

---
+++ src/main/java/onlydust/com/marketplace/kernel/infrastructure/postgres/PostgresOutboxAdapter.java
@@ -0,0 +1,63 @@
+package onlydust.com.marketplace.kernel.infrastructure.postgres;
+
+import lombok.AllArgsConstructor;
+import onlydust.com.marketplace.kernel.model.Event;
+import onlydust.com.marketplace.kernel.port.output.OutboxPort;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+import static onlydust.com.marketplace.kernel.exception.OnlyDustException.internalServerError;
+
+@AllArgsConstructor
+public class PostgresOutboxAdapter<E extends EventEntity> implements OutboxPort {
+
+    private final OutboxRepository<E> outboxRepository;
+
+    @Override
+    @Transactional
+    public void push(Event event) {
+        outboxRepository.saveEvent(event);
+    }
+
+    @Override
+    @Transactional
+    public Optional<IdentifiableEvent> peek() {
+        return outboxRepository.findNextToProcess()
+                .map(e -> {
+                    e.setStatus(EventEntity.Status.PROCESSING);
+                    return e;
+                })
+                .map(EventEntity::toIdentifiableEvent);
+    }
+
+    @Override
+    @Transactional
+    public void ack(Long eventId) {
+        final var entity = outboxRepository.selectForUpdateById(eventId)
+                .orElseThrow(() -> internalServerError("Event %d not found".formatted(eventId)));
+
+        entity.setStatus(EventEntity.Status.PROCESSED);
+        entity.setError(null);
+    }
+
+    @Override
+    @Transactional
+    public void nack(Long eventId, String message) {
+        final var entity = outboxRepository.selectForUpdateById(eventId)
+                .orElseThrow(() -> internalServerError("Event %d not found".formatted(eventId)));
+
+        entity.setStatus(EventEntity.Status.FAILED);
+        entity.setError(message);
+    }
+
+    @Override
+    @Transactional
+    public void skip(Long eventId, String message) {
+        final var entity = outboxRepository.selectForUpdateById(eventId)
+                .orElseThrow(() -> internalServerError("Event %d not found".formatted(eventId)));
+
+        entity.setStatus(EventEntity.Status.SKIPPED);
+        entity.setError(message);
+    }
+}
